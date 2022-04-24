575b2ac82d7e1992292a741bd11941578dedd3e5
Ahmed attia <toosoo234@gmail.com>
2022-04-24 23:58:54 +0200

    space 1.1

--- src/app.js
+++ src/app.js
@@ -12,12 +12,10 @@ gsap.from(".m-2", {
   x: "110vw",
   duration: 1,
   repeat: -1,
-  delay:1,
+  delay: 1,
   repeatDelay: 5,
 });
-let posX = Math.floor(Math.random() * 200);
-let posY = Math.floor(Math.random() * 200);
-let body = document.querySelector("body")
+let body = document.querySelector("body");
 for (let i = 0; i < 50; i++) {
   let star = document.createElement("span");
   star.setAttribute("class", "star");
@@ -31,24 +29,23 @@ gsap.to(".star", {
   opacity: 0.1,
   duration: 0.7,
   repeat: -1,
-  stagger:.08,
   yoyo: true,
   ease: "linear",
 });
 
 let planets = gsap.utils.toArray(".planet");
-    planetDetails = document.querySelector(".planet-details");
-    planetsContainer = document.querySelector(".planets-container");
-    stars = gsap.utils.toArray(".star");
-    blackhole = document.querySelector(".blackhole")
-  
+let planetDetails = document.querySelector(".planet-details");
+let planetsContainer = document.querySelector(".planets-container");
+let stars = gsap.utils.toArray(".star");
+let layout = document.querySelector(".layout");
+let dynamic = gsap.utils.toArray(".main , .planet , .planets-container");
+
 planets.forEach((planet) => {
   planet.addEventListener("click", (e) => {
     let state = Flip.getState(".planet");
 
     if (planet.parentNode === planetsContainer && planetDetails.children.length === 0) {
       planetDetails.appendChild(planet);
-     
     } else if (planet.parentNode === planetsContainer && planetDetails.children.length === 1) {
       let activePlanet = document.querySelector(" .planet-details .planet");
       planetsContainer.appendChild(activePlanet);
@@ -62,13 +59,13 @@ planets.forEach((planet) => {
       absolute: true,
       scale: true,
     });
-    if(planetDetails.children.length === 1) {
+    if (planetDetails.children.length === 1) {
       changeText();
     } else {
-        originalText()
+      originalText();
     }
 
-    gsap.utils.shuffle(stars)
+    gsap.utils.shuffle(stars);
   });
 });
 
@@ -86,11 +83,10 @@ function changeText() {
     text: {
       value: aboutPlanet,
     },
-    delay:.5,
+    delay: 0.5,
     duration: 2,
     ease: "power1.inOut",
   });
-
 }
 
 function originalText() {
@@ -105,38 +101,21 @@ function originalText() {
     text: {
       value: "Click on any planet",
     },
-    delay:.5,
+    delay: 0.5,
     duration: 2,
     ease: "power1.inOut",
   });
 }
 
-// planetsContainer,planetDetails,".main",planets,
+layout.addEventListener("click", () => {
+  let state2 = Flip.getState(dynamic);
 
-blackhole.addEventListener("click",()=>{
-    let state2 = Flip.getState(body ,{props:"flexDirection"});
-body.classList.toggle("row-design")
-  
-if(body.classList.contains("row-design")) {
-    body.style = "flex-direction:row";
-    // planetsContainer.style = " width: 8vw; height: 90%;flex-direction: column;";
-    // document.querySelector(".main").style = " width: 90vw;"
-}
+  body.classList.toggle("row-design");
 
-else {
-    body.style = "flex-direction:column";
-    // planetsContainer.style = " width: 80vw;height: 10%;;flex-direction: row;";
-    // document.querySelector(".main").style = " width: 98vw;";
-}
-Flip.from(state2, {
+  Flip.from(state2, {
     duration: 1,
+    absolute: true,
     ease: "power4.inOut",
-
-    scale:true,
-    nested: true
+    nested: true,
   });
-
-})
-
-
-
+});
